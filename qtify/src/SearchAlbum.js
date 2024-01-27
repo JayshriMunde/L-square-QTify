d873fc5a7fdcbd16d989a656253983763a4e9263
DHANANJAY NAGARGOJE <dhananjay.nagargoje1@t-mobile.com>
2024-01-27 18:52:31 +0530

    Navbar

--- qtify/src/SearchAlbum.js
+++ qtify/src/SearchAlbum.js
@@ -8,7 +8,7 @@ function SearchAlbum() {
       <div className="search-input">
         <input type="text" placeholder="Search a song of your choice" />
         <img
-          src="assests/search icon.png"
+          src="assests/Search_icon.png"
           alt="Figma Icon"
           className="search-icon"
         />
